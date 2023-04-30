2d964017b736cf79a5b2bde192caafae1c608262
IdoKendo <ryuusuke@gmail.com>
2023-04-30 09:49:13 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# dotfiles
+My personal dotfiles
+
+## Pre-requisite
+`sudo apt install stow`
+
+## Installation
+1. Clone the repo to your `$HOME` directory: `git@github.com:IdoKendo/dotfiles.git`
+2. Run stow on the folders: `cd ~/.dotfiles && stow nvim && stow tmux && stow starship`
---
+++ tmux/cht.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+languages=$(echo "golang c cpp python tpyscript javascript rust bash html css swift" | tr " " "\n")
+core_utils=$(echo "find xargs sed awk grep" | tr " " "\n")
+selected=$(echo -e "$languages\n$core_utils" | fzf)
+
+read -p "Query: " query
+
+if echo "$languages" | grep -qs $selected; then
+    tmux split-window -h bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less -R"
+else
+    tmux split-window -h bash -c "curl cht.sh/$selected~$query | less -R"
+fi
