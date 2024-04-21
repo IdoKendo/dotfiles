b2c1d84c8f9295c2e0f82e7253bbbb3c82fbbc05
IdoKendo <ryuusuke@gmail.com>
2024-04-21 17:18:37 +0300

    feat: replace host in tmux status to battery percent

---
+++ scripts/.local/scripts/batt.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+BATTERY_PCT=$(pmset -g batt | grep % | awk 'NR=1{print $3}' | cut -c 1-2)
+BATTERY_ICON=""
+if [[ $BATTERY_PCT -gt 90 ]] then
+    BATTERY_ICON="󰁹"
+elif [[ $BATTERY_PCT -gt 80 ]] then
+    BATTERY_ICON="󰂂"
+elif [[ $BATTERY_PCT -gt 70 ]] then
+    BATTERY_ICON="󰂁"
+elif [[ $BATTERY_PCT -gt 60 ]] then
+    BATTERY_ICON="󰂀"
+elif [[ $BATTERY_PCT -gt 50 ]] then
+    BATTERY_ICON="󰁿"
+elif [[ $BATTERY_PCT -gt 40 ]] then
+    BATTERY_ICON="󰁾"
+elif [[ $BATTERY_PCT -gt 30 ]] then
+    BATTERY_ICON="󰁽"
+elif [[ $BATTERY_PCT -gt 20 ]] then
+    BATTERY_ICON="󰁼"
+elif [[ $BATTERY_PCT -gt 10 ]] then
+    BATTERY_ICON="󰁻"
+else
+    BATTERY_ICON="󰁺"
+fi
+
+echo "$BATTERY_ICON $BATTERY_PCT%"
