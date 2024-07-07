0ff15b6c23b4765d6fa09160a65aced7409257d4
IdoKendo <ryuusuke@gmail.com>
2024-07-07 15:30:29 +0300

    chore: split configuration.nix to a separate file

--- nix/.config/nix-darwin/flake.nix
+++ nix/.config/nix-darwin/flake.nix
@@ -4,73 +4,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
-
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin= {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
-          pkgs.coreutils
-        ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Enable touch ID to unlock sudo commands
-      security.pam.enableSudoTouchIdAuth = true;
-      # This also support touch ID in tmux to unlock sudo
-      environment.etc."pam.d/sudo_local".text = ''
-        # Managed by Nix Darwin
-        auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
-        auth       sufficient     pam_tid.so
-      '';
-
-      # Set up FiraCode nerd font
-      fonts.packages = [ (pkgs.nerdfonts.override { fonts = ["FiraCode"]; }) ];
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Greeting text on login window
-      system.defaults.loginwindow.LoginwindowText = "Ido S.\nSenior Software Engineer";
-
-      # Finder display settings
-      system.defaults.finder.AppleShowAllExtensions = true;
-      system.defaults.finder.AppleShowAllFiles = true;
-      system.defaults.finder._FXShowPosixPathInTitle = true;
-      system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
-      system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Idos-MacBook-Pro
     darwinConfigurations."Idos-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ "${self}/configuration.nix" ];
     };
 
     # Expose the package set, including overlays, for convenience.
