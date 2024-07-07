abd0b91765dfd46581db80b08aff3e6446005b42
IdoKendo <ryuusuke@gmail.com>
2024-07-07 09:20:23 +0300

    feat: install nerd fonts and set up some settings

--- nix/.config/nix-darwin/flake.nix
+++ nix/.config/nix-darwin/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example Darwin system flake";
+  description = "Ido's system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -14,7 +14,7 @@
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [
-          pkgs.vim
+          pkgs.coreutils
         ];
 
       # Auto upgrade nix package and the daemon service.
@@ -37,11 +37,21 @@
         auth       sufficient     pam_tid.so
       '';
 
+      # Set up FiraCode nerd font
+      fonts.packages = [ (pkgs.nerdfonts.override { fonts = ["FiraCode"]; }) ];
+
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
       # Greeting text on login window
       system.defaults.loginwindow.LoginwindowText = "Ido S.\nSenior Software Engineer";
+
+      # Finder display settings
+      system.defaults.finder.AppleShowAllExtensions = true;
+      system.defaults.finder.AppleShowAllFiles = true;
+      system.defaults.finder._FXShowPosixPathInTitle = true;
+      system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+      system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
