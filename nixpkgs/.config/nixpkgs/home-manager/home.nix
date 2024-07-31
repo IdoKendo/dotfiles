9451696f113f4331f3e5d178a16cc7325102b315
IdoKendo <ryuusuke@gmail.com>
2024-07-31 11:55:04 -0400

    feat: initial nixos setup

---
+++ nixpkgs/.config/nixpkgs/home-manager/home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, unstable, ... }:
+{
+  home = {
+    username = "idoslonimsky";
+    homeDirectory = "/home/idoslonimsky";
+    stateVersion = "24.05";
+    packages = [
+      pkgs.cargo
+      pkgs.gitui
+      pkgs.gcc
+      pkgs.lua51Packages.jsregexp
+      pkgs.lua51Packages.lua
+      pkgs.luarocks
+      unstable.neovim
+      pkgs.nodePackages.prettier
+      pkgs.nodejs
+      unstable.uv
+    ];
+  };
+
+  imports = [
+    ./modules/bundle.nix
+  ];
+
+  xdg.enable = true;
+
+  programs = {
+    eza.enable = true;
+    home-manager.enable = true;
+  };
+}
---
+++ nixpkgs/.config/nixpkgs/nixos/packages.nix
@@ -0,0 +1,59 @@
+{ pkgs, ... }: {
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    alacritty
+    aws-iam-authenticator
+    bat
+    cmake
+    cmatrix
+    coreutils
+    curl
+    d2
+    eksctl
+    execline
+    fastfetch
+    fd
+    fzf
+    gettext
+    git
+    gnupg
+    gnused
+    gnutls
+    go
+    golangci-lint
+    home-manager
+    hurl
+    jq
+    k9s
+    kind
+    kubebuilder
+    kubecolor
+    kubectl
+    kubectx
+    kubernetes-helm
+    less
+    lua51Packages.jsregexp
+    lua51Packages.lua
+    luarocks
+    pwgen
+    ripgrep
+    slirp4netns
+    silicon
+    sqlc
+    stow
+    unzip
+    uv
+    wget
+    xclip
+    xdg-ninja
+    yq
+  ];
+
+  # Install custom fonts.
+  fonts.packages = [ (pkgs.nerdfonts.override { fonts = ["FiraCode"]; }) ];
+}
