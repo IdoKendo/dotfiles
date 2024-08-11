017d2f653fe70d9fcc775952c87d0b11b1ff3265
IdoKendo <ryuusuke@gmail.com>
2024-08-11 09:51:46 +0300

    fix: make batt script OS agnostic (kinda)

--- scripts/.local/scripts/batt.sh
+++ scripts/.local/scripts/batt.sh
@@ -1,27 +1,25 @@
-#!/bin/zsh
-
-BATTERY_PCT=$(pmset -g batt | grep % | awk 'NR=1{print $3}' | cut -c 1-2)
-if [[ ! "$BATTERY_PCT" ]] then
-    BATTERY_PCT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk 'NR=1{print $2}' | cut -c 1-2)
-fi
+case "$(uname -s)" in
+    Linux*) BATTERY_PCT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk 'NR=1{print $2}' | cut -c 1-2);;
+    Darwin*) BATTERY_PCT=$(pmset -g batt | grep % | awk 'NR=1{print $3}' | cut -c 1-2);;
+esac
 BATTERY_ICON=""
-if [[ $BATTERY_PCT -gt 90 ]] then
+if [[ $BATTERY_PCT -gt 90 ]]; then
     BATTERY_ICON="󰁹"
-elif [[ $BATTERY_PCT -gt 80 ]] then
+elif [[ $BATTERY_PCT -gt 80 ]]; then
     BATTERY_ICON="󰂂"
-elif [[ $BATTERY_PCT -gt 70 ]] then
+elif [[ $BATTERY_PCT -gt 70 ]]; then
     BATTERY_ICON="󰂁"
-elif [[ $BATTERY_PCT -gt 60 ]] then
+elif [[ $BATTERY_PCT -gt 60 ]]; then
     BATTERY_ICON="󰂀"
-elif [[ $BATTERY_PCT -gt 50 ]] then
+elif [[ $BATTERY_PCT -gt 50 ]]; then
     BATTERY_ICON="󰁿"
-elif [[ $BATTERY_PCT -gt 40 ]] then
+elif [[ $BATTERY_PCT -gt 40 ]]; then
     BATTERY_ICON="󰁾"
-elif [[ $BATTERY_PCT -gt 30 ]] then
+elif [[ $BATTERY_PCT -gt 30 ]]; then
     BATTERY_ICON="󰁽"
-elif [[ $BATTERY_PCT -gt 20 ]] then
+elif [[ $BATTERY_PCT -gt 20 ]]; then
     BATTERY_ICON="󰁼"
-elif [[ $BATTERY_PCT -gt 10 ]] then
+elif [[ $BATTERY_PCT -gt 10 ]]; then
     BATTERY_ICON="󰁻"
 else
     BATTERY_ICON="󰁺"
