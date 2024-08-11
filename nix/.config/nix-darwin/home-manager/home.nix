6f2cabc54e669c51de4663306fd191138874485b
IdoKendo <ryuusuke@gmail.com>
2024-08-11 13:27:09 +0300

    chore: move common files to commn directory

--- nix/.config/nix-darwin/home-manager/home.nix
+++ nix/.config/nix-darwin/home-manager/home.nix
@@ -6,7 +6,11 @@
   };
 
   imports = [
-    ./modules/bundle.nix
+    ../common/alacritty.nix
+    ../common/atuin.nix
+    ../common/ruff.nix
+    ../common/starship.nix
+    ../common/tmux.nix
   ];
 
   xdg.enable = true;
