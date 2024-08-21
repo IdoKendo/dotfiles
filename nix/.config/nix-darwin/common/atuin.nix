69f6d077b4979259819ac4e5a4bf0455612c2b02
IdoKendo <ryuusuke@gmail.com>
2024-08-21 09:50:07 +0300

    feat: add a few more history filters to atuin

--- nix/.config/nix-darwin/common/atuin.nix
+++ nix/.config/nix-darwin/common/atuin.nix
@@ -6,6 +6,9 @@
       keymap_mode = "vim-insert";
       enter_accept = true;
       history_filter = [
+         "^cat "
+         "^cd "
+         "^ll"
          "^v \.$"
          "^which "
       ];
