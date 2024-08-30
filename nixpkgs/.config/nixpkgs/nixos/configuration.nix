aa50536230d9ff4aa279dd08a684e49a0c6827a3
IdoKendo <ryuusuke@gmail.com>
2024-08-30 04:29:00 -0400

    feat: reuse meta.hostname

--- nixpkgs/.config/nixpkgs/nixos/configuration.nix
+++ nixpkgs/.config/nixpkgs/nixos/configuration.nix
@@ -15,7 +15,7 @@
     use-xdg-base-directories = true;
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = meta.hostname;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
