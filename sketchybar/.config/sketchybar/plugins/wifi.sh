f1d32a8e374faad775a9905bf84a365e9d04f70b
IdoKendo <ryuusuke@gmail.com>
2025-10-30 11:02:08 +0200

    fix(sketchybar): network name

--- sketchybar/.config/sketchybar/plugins/wifi.sh
+++ sketchybar/.config/sketchybar/plugins/wifi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-INFO="$(system_profiler SPAirPortDataType | awk '/Current Network/ {getline;$1=$1;print $0 | "tr -d ':'";exit}')"
+INFO="$(en="$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{getline; print $NF}')"; ipconfig getsummary "$en" | grep -Fxq "  Active : FALSE" || networksetup -listpreferredwirelessnetworks "$en" | sed -n '2s/^\t//p')"
 IP="$(ipconfig getifaddr en0)"
 
 ICON="$([ -n "$IP" ] && echo "" || echo "󱛅")"
 LABEL="$([ -n "$IP" ] && echo "$INFO" || echo "Disconnected")"
 
-sketchybar --set $NAME icon.color=0x44ffffff label.color=0x44ffffff icon="$ICON" label="$LABEL"
+sketchybar --set "$NAME" icon.color=0x44ffffff label.color=0x44ffffff icon="$ICON" label="$LABEL"
