ba65ad93c744b1ccfb2ed33d61f9e13eade6168e
IdoKendo <ryuusuke@gmail.com>
2025-11-09 13:10:03 +0200

    fix: batt script to work nicely on desktops

--- scripts/.local/scripts/batt.sh
+++ scripts/.local/scripts/batt.sh
@@ -1,6 +1,25 @@
 case "$(uname -s)" in
-    Linux*) BATTERY_PCT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk 'NR=1{print $2}' | cut -c 1-2);;
-    Darwin*) BATTERY_PCT=$(pmset -g batt | grep % | awk 'NR=1{print $3}' | cut -c 1-2);;
+    Linux*) 
+        BATTERY_DEVICES=$(upower -e 2>/dev/null | grep battery)
+        VALID_BATTERY=""
+
+        for device in $BATTERY_DEVICES; do
+            DEVICE_INFO=$(upower -i "$device" 2>/dev/null)
+            if echo "$DEVICE_INFO" | grep -q "power supply: yes"; then
+                VALID_BATTERY="$device"
+                BATTERY_INFO="$DEVICE_INFO"
+                break
+            fi
+        done
+
+        if [[ -z "$VALID_BATTERY" ]]; then
+            echo "󰚥 AC"
+            exit 0
+        fi
+
+        BATTERY_PCT=$(echo "$BATTERY_INFO" | grep percentage | awk 'NR=1{print $2}' | sed 's/%//')
+        ;;
+    Darwin*) BATTERY_PCT=$(pmset -g batt | grep % | awk 'NR=1{print $3}' | sed 's/%;//');;
 esac
 BATTERY_ICON=""
 if [[ $BATTERY_PCT -gt 90 ]]; then
@@ -25,4 +44,4 @@ else
     BATTERY_ICON="󰁺"
 fi
 
-echo "$BATTERY_ICON $BATTERY_PCT%"
+echo "$BATTERY_ICON ${BATTERY_PCT}%"
