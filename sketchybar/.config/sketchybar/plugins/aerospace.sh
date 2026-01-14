0a14acdee1af455d70c5c098c72dacf5d4272bde
IdoKendo <ryuusuke@gmail.com>
2026-01-14 09:53:45 +0200

    fix(sketchybar): minor change to the way aerospace is rendered

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -7,7 +7,7 @@ if [[ -z "$FOCUSED_WORKSPACE" ]]; then
     focused_space=$(aerospace list-workspaces --focused)
     sketchybar --set space."$focused_space" background.drawing=on label.color=$BG_SECONDARY label="$focused_space [$(aerospace list-windows --workspace focused | wc -l | grep -Eo '\d')]"
 elif [[ "$1" = "$FOCUSED_WORKSPACE" ]]; then
-    sketchybar --set "$NAME" background.drawing=on label.color=$BG_SECONDARY label="$1 [$(aerospace list-windows --focused | awk -F '|' '{print $2}' | xargs)]"
+    sketchybar --set "$NAME" background.drawing=on label.color=$BG_SECONDARY label="$1 [$(aerospace list-windows --focused | awk '{print $3}')]"
 else
     sketchybar --set "$NAME" background.drawing=off label.color=$BLUE label="$(echo $NAME | grep -Eo '[0-9]{1,4}')"
 fi
